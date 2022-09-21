2d15bed482097ebf1ff15f4beb1e81019310bd55
lakshmankumar <laxmansgvp@gmail.com>
2022-09-21 10:11:38 +0530

    adding file to master

---
+++ Day4Assignments/Day4Problems/Rolldice.java
@@ -0,0 +1,20 @@
+package Day4Assignments.Day4Problems;
+
+
+class dice {
+	public int rollDice()
+	{
+	    rollDice = (int)math.floar(math.random())
+	}
+	
+	
+}
+public class Rolldice {
+	
+	
+	}
+
+
+	
+
+
---
+++ Day4Assignments/Day4Problems/SnakeAndLadderPlayer.java
@@ -0,0 +1,20 @@
+package Day4Assignments.Day4Problems;
+class player{
+	int position;
+	player(){
+		this.position=0;
+	}
+	void display() {
+		System.out.println(position);
+	}
+}
+public class SnakeAndLadderPlayer {
+	public static void main(String[] args) {
+		player p = new player();
+		p.display();
+		
+	}
+	
+	
+
+}
